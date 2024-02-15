c7ea09dcdc77442a9536eb8f9a62c9f767e6a230
Taro MURAOKA <koron@juno.kaoriya.net>
2024-02-16 08:47:45 +0900

    improve initiation scripts
    - ORGNAME can be specified
    - PROJNAME can be specified
    - dryrun option
    - delete option: delete files which not needed after initialization

--- _initialize_project.sh
+++ _initialize_project.sh
@@ -5,6 +5,32 @@ set -eu
 CURDIR=$(cd $(dirname $0); pwd)
 ORGNAME=$(basename $(dirname ${CURDIR}))
 PROJNAME=$(basename ${CURDIR})
+DRYRUN=no
+DELETE=no
+
+while getopts dno:p: OPT ; do
+  case $OPT in
+    d)
+      DELETE=yes
+      ;;
+    n)
+      DRYRUN=yes
+      ;;
+    o)
+      ORGNAME="$OPTARG"
+      ;;
+    p)
+      PROJNAME="$OPTARG"
+      ;;
+  esac
+done
+
+if [ "$DRYRUN" = "yes" ] ; then
+  echo ORGNAME:  $ORGNAME
+  echo PROJNAME: $PROJNAME
+  echo DELETE:   $DELETE
+  exit 1
+fi
 
 #name=$1 ; shift
 
@@ -13,4 +39,7 @@ grep -lr '{{\.\(Name\|Org\)}}' . | xargs sed -i.bak \
   -e "s/{{\.Org}}/${ORGNAME}/g" 
 find . -type f -name \*.bak | xargs rm
 
-#rm -f $0
+if [ "$DELETE" = "yes" ] ; then
+  rm -f $0
+  rm -rf .git
+fi
